b5d6bb77c92e288b55677bc8f07edf31a395c4ea
J-Yujiaixin <1269943206@qq.com>
2022-03-15 19:39:25 +0800

    'v2.3'

---
+++ components/responseInfo/responseInfo.vue
@@ -0,0 +1,69 @@
+<template>
+	<scroll-view scroll-x="false" scroll-y="true">
+		<view v-for="(item,index) in cards" @click="clickCard()">
+			<uni-card style="height: 300rpx;" :title="item.title" extra="额外信息">
+				<view class="cardview">
+				<image style="width:150rpx;height: 150rpx;" :src="item.url"></image>
+				<view style="width:50rpx"></view>
+			    <text>{{item.description}}</text>
+				</view>
+			</uni-card>
+		</view>	
+	</scroll-view>
+</template>
+
+<script>
+	export default {
+		name:"responseInfo",
+		data() {
+			return {
+				cards:[
+					{
+						title:"有害垃圾",
+						description:"酒精",
+						url:"../../static/logo.png",
+					},
+					{
+						title:"有害垃圾",
+						description:"酒精",
+						url:"../../static/logo.png",
+					},
+					{
+						title:"有害垃圾",
+						description:"酒精",
+						url:"../../static/logo.png",
+					},
+					{
+						title:"有害垃圾",
+						description:"酒精",
+						url:"../../static/logo.png",
+					},
+					{
+						title:"有害垃圾",
+						description:"酒精",
+						url:"../../static/logo.png",
+					},
+					{
+						title:"有害垃圾",
+						description:"酒精",
+						url:"../../static/logo.png",
+					},
+				],
+				
+			};
+		}
+	}
+</script>
+
+<style>
+	.cardview{
+		height: 200rpx;
+		bottom: 0;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
+	scroll-view{
+		background-color: #F0F0F0;
+	}
+</style>
--- pages/Requestinfo/Requestinfo.vue
+++ pages/Requestinfo/Requestinfo.vue
@@ -1,6 +1,18 @@
 <template>
-	<view>
-		识别结果信息页
+	<view class="content">
+		<view class="header">
+			<view style="display: flex;font-size:35rpx;align-items: center;justify-content: center;">
+			验证结果<view style="font-size: 25rpx;display: flex;align-items: center;justify-content: center;">({{imgNum}})</view>
+			</view>
+			<view class="iconfont ">
+			</view>
+		</view>
+		<view class="scroll">
+		<view style="height:100rpx;"></view>
+		<responseInfo></responseInfo>
+		<view style="height:200rpx;"></view>
+		</view>
+		<view class="footer">这是一个注脚</view>
 	</view>
 </template>
 
@@ -12,6 +24,8 @@
 		},
 		data() {
 			return {
+				imgNum:0,
+				
 				
 			}
 		},
@@ -22,5 +36,65 @@
 </script>
 
 <style>
+	.content{
+		width:100%;
+		height:100vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		align-items: center;
+		background-color: #F0F0F0;
+	}
+	.header{
+		height:8%;
+		width:100%;
+		display: flex;
+		align-items: center;
+		justify-content: space-around;
+		position:fixed;
+		top:0;
+		z-index: 199;
+		color:#000000;
+		font-size: 40rpx;
+		background-color: #FFFFFF;
+	}
+	.scroll{
+		position:absolute;
+		top:10%;
+		bottom: 10%;
+		width:95%;
+		background-color: #F0F0F0;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	.footer{
+		height:8%;
+		width:100%;
+		color:#D8D8D8;
+		position: fixed;
+		bottom: 0;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		z-index: 199;
+		background-color: #FFFFFF;
+	}
+	.cardview{
+		height: 200rpx;
+		bottom: 0;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
+	text{
+		display: flex;
+		width:400rpx;
+		justify-content: center;
+		align-items: ;
+	}
+	
+	
+	
 
 </style>
